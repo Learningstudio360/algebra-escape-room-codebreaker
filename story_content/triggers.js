ea5cbe102e83a187be5c7a5ff078da15e5402cc9
Learningstudio360 <radiantvibe.apparel@gmail.com>
2026-03-30 12:53:34 -0700

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,206 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6HmjEco2XWT":
+        Script1();
+        break;
+      case "6D5ihXxxcsF":
+        Script2();
+        break;
+      case "6m3BMuu8za6":
+        Script3();
+        break;
+      case "6ncd6uYB3mR":
+        Script4();
+        break;
+      case "6SlKimkmsOe":
+        Script5();
+        break;
+      case "5x8jL6yp5b8":
+        Script6();
+        break;
+      case "6kGqSWO3vtQ":
+        Script7();
+        break;
+      case "6mWg7RZyFm2":
+        Script8();
+        break;
+      case "6bv2MqCfCWJ":
+        Script9();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script1 = function()
+{
+  const target = object('6XnFfQa2PHN');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6C7sotC9Tvh';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('5wEziazxnaL');
+const duration = 750;
+const easing = 'ease-out';
+const id = '63TgEKaTXM5';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script3 = function()
+{
+  const target = object('6iQZNyrCQ1p');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6We4PnCWPnb';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script4 = function()
+{
+  const target = object('6qtEr41Xmmb');
+const duration = 750;
+const easing = 'ease-out';
+const id = '63kqocp5pwh';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script5 = function()
+{
+  const target = object('6iT4dzLD2l5');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6izzq7GhSdH';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script6 = function()
+{
+  const target = object('6LDijqH5mh5');
+const duration = 750;
+const easing = 'ease-out';
+const id = '63JmGyB610O';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script7 = function()
+{
+  const target = object('5g54CTAIJ8R');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6Mn6KZtIQyJ';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script8 = function()
+{
+  const target = object('6MQ7xFiGmFM');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6bBg7Z5I0pf';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script9 = function()
+{
+  const target = object('5hDIiZ8s3ql');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5nToZKKAkxC';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
